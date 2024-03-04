0e72e05e91daa7366588de8ac367686507b361d2
adarshjha7 <adarshjha7035@gmail.com>
2024-03-04 17:45:24 +0530

    first commit

--- public/client.js
+++ public/client.js
@@ -28,16 +28,21 @@ function sendMessage(message) {
 }
 
 function appendMessage(msg, type) {
-    let mainDiv = document.createElement('div')
-    let className = type
-    mainDiv.classList.add(className, 'message')
+    let mainDiv = document.createElement('div');
+    let className = type;
+    mainDiv.classList.add(className, 'message');
 
     let markup = `
         <h4>${msg.user}</h4>
         <p>${msg.message}</p>
-    `
-    mainDiv.innerHTML = markup
-    messageArea.appendChild(mainDiv)
+    `;
+
+    if (type === 'outgoing') {
+        markup += `<span class="user-tag"></span>`;
+    }
+
+    mainDiv.innerHTML = markup;
+    messageArea.appendChild(mainDiv);
 }
 
 // Recieve messages 
